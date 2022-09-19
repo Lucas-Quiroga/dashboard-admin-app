8bbe7aed29ad95b74e08ef80f4d55f9640ce347f
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2022-09-19 19:33:42 -0300

    setup component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 function App() {
-  const activeMenu = true;
+  const activeMenu = false;
   return (
     <BrowserRouter>
       <div className="flex realtive dark:bg-main-dark-bg">
@@ -35,6 +35,35 @@ function App() {
           <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
             Navbar
           </div>
+        </div>
+
+        <div>
+          <Routes>
+            {/* Dashboard */}
+            <Route path="/" element="ECommerce" />
+            <Route path="/ecommerce" element="ECommerce" />
+
+            {/* Pages */}
+            <Route path="/orders" element="Orders" />
+            <Route path="/employees" element="Employees" />
+            <Route path="/customers" element="Customers" />
+
+            {/* Apps */}
+            <Route path="/kanban" element="Kanban" />
+            <Route path="/editor" element="Editor" />
+            <Route path="/calendar" element="Calendar" />
+            <Route path="/color-picker" element="ColorPicker" />
+
+            {/* Charts */}
+            <Route path="/line" element="Line" />
+            <Route path="/area" element="Area" />
+            <Route path="/bar" element="Bar" />
+            <Route path="/pie" element="Pie" />
+            <Route path="/financial" element="Financial" />
+            <Route path="/color-mapping" element="ColorMapping" />
+            <Route path="/pyramid" element="Pyramid" />
+            <Route path="/stacked" element="Stacked" />
+          </Routes>
         </div>
       </div>
     </BrowserRouter>
